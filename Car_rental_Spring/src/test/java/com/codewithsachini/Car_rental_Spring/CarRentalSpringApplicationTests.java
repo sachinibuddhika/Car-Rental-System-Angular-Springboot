b0c8896d1ce9082ad47e95f19b6f7d2ac8276356
Sachini Buddhika <sachiniste3@gmail.com>
2025-01-28 12:12:28 +0530

    initial commit

---
+++ Car_rental_Spring/src/test/java/com/codewithsachini/Car_rental_Spring/CarRentalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithsachini.Car_rental_Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
